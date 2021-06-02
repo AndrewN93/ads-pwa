d52d41fc3b1eb6fc7f6cff3d7ef635f43b1a20a1
Andrey Nechmoglod <andrey.nechmoglod@nixsolutions.com>
2021-06-02 10:34:02 +0300

    prompting user on new version deploy config added

---
+++ src/app/prompt-update.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PromptUpdateService {
+  constructor(updates: SwUpdate) {
+    updates.available.subscribe(() => {
+      if (this.promptUser()) {
+        updates.activateUpdate().then(() => document.location.reload());
+      }
+    });
+  }
+
+  promptUser() {
+    return prompt('There is an update of the app available, please reload the page=)')
+  }
+}
+
+
